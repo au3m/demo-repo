369b86deb43bc2164c07736138aa25808c06d0dc
ahmed <ahmed29yousry@gmail.com>
2024-07-20 02:15:10 +0300

    commit1

---
+++ src/main/java/com/u3/demospringsecurityproject/DemospringsecurityprojectApplication.java
@@ -0,0 +1,14 @@
+package com.u3.demospringsecurityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringsecurityprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringsecurityprojectApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/u3/demospringsecurityproject/DemospringsecurityprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.u3.demospringsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringsecurityprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
